cba865dbfc105e81e5ede6286eab6ba999484898
JEFF-MIKE <michaeljefferies08@gmail.com>
2024-08-22 00:03:42 +0100

    Use a sample image modifer for actual testing

--- tests/widgets/test_slider_manager.py
+++ tests/widgets/test_slider_manager.py
@@ -2,6 +2,7 @@
 
 import pytest
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
 from light_detection.widgets.slider_manager import SliderManager
 from light_detection.dataclasses.image_dataclasses import ImageModifiers
 
@@ -11,44 +12,45 @@ def slider_manager():
     def dummy_callback(value):
         pass
 
+    custom_image_modifiers = ImageModifiers(
+        binary_threshold_value=131,
+        adaptive_block_size=27,
+        adaptive_constant_c=2,
+        blur_value=100,
+    )
+
     return SliderManager(
-        image_modifiers=ImageModifiers(),
+        image_modifiers=custom_image_modifiers,
         slider_value_change_callback=dummy_callback,
+        blur_callback=dummy_callback,
     )
 
 
 def test_slider_manager_initialization(slider_manager):
+    # Check for core slider manager properties (e.g. what the user will see on creation)
     assert isinstance(slider_manager, SliderManager)
     assert isinstance(slider_manager.image_modifiers, ImageModifiers)
     assert callable(slider_manager.slider_change_callback)
+    assert callable(slider_manager.blur_callback)
+    assert isinstance(slider_manager.blur_box, GridLayout)
     assert isinstance(slider_manager.slider_label_row, BoxLayout)
     assert isinstance(slider_manager.slider_row, BoxLayout)
+    assert slider_manager.blur_switch_container.children != []
+    assert slider_manager.blur_slider_container.children == []
 
 
 def test_slider_initial_values(slider_manager):
-    assert (
-        slider_manager.binary_threshold_slider.value
-        == slider_manager.image_modifiers.binary_threshold_value
-    )
-    assert (
-        slider_manager.block_size_slider.value
-        == slider_manager.image_modifiers.adaptive_block_size
-    )
-    assert (
-        slider_manager.constant_c_slider.value
-        == slider_manager.image_modifiers.adaptive_constant_c
-    )
+    assert slider_manager.binary_threshold_slider.value == 131
+    assert slider_manager.block_size_slider.value == 27
+    assert slider_manager.constant_c_slider.value == 2
+    assert slider_manager.blur_slider.value == 100
 
 
 def test_slider_labels_initial_values(slider_manager):
-    assert (
-        slider_manager.binary_threshold_label.text
-        == f"Pixel Threshold: {str(slider_manager.binary_threshold_slider.value)}"
-    )
-    assert (
-        slider_manager.block_size_slider_label.text
-        == f"Block Size: {str(slider_manager.block_size_slider.value)}"
-    )
+    assert slider_manager.binary_threshold_label.text == f"Pixel Threshold: 131"
+    assert slider_manager.block_size_slider_label.text == f"Block Size: 27"
+    assert slider_manager.constant_c_slider_label.text == f"Constant C: 2"
+    assert slider_manager.blur_label_value.text == f"Blur Value: 100"
 
 
 def test_add_adaptive_sliders(slider_manager):
@@ -98,4 +100,24 @@ def test_remove_binary_threshold_slider(slider_manager):
     assert (
         slider_manager.binary_threshold_label
         not in slider_manager.slider_label_row.children
+    )
+
+
+def test_attach_blur_slider(slider_manager):
+    slider_manager.attach_blur_slider()
+    assert slider_manager.blur_slider in slider_manager.blur_slider_container.children
+    assert (
+        slider_manager.blur_label_value in slider_manager.blur_slider_container.children
+    )
+
+
+def test_remove_blur_slider(slider_manager):
+    slider_manager.attach_blur_slider()
+    slider_manager.remove_blur_slider()
+    assert (
+        slider_manager.blur_slider not in slider_manager.blur_slider_container.children
+    )
+    assert (
+        slider_manager.blur_label_value
+        not in slider_manager.blur_slider_container.children
     )
